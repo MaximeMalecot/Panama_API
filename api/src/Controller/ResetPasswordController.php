87fc8151054a0dd688e0ef487f769ec771732779
Chaochao <chaochao.zhou@hotmail.com>
2023-01-18 12:01:36 +0100

    Send mail reset Password

--- api/src/Controller/ResetPasswordController.php
+++ api/src/Controller/ResetPasswordController.php
@@ -3,9 +3,13 @@ namespace App\Controller;
 
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use phpDocumentor\Reflection\Types\Context;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
 
 #[AsController]
 class ResetPasswordController extends AbstractController
@@ -13,19 +17,25 @@ class ResetPasswordController extends AbstractController
 
     public function __construct(private RequestStack $requestStack, private EntityManagerInterface $em){}
 
-    public function __invoke()
+    public function __invoke(MailerInterface $mailer)
     {
         $email = json_decode($this->requestStack->getCurrentRequest()->getContent())->email;
 
         if(!$user = $this->em->getRepository(User::class)->findOneBy(['email' => $email])){
             throw $this->createNotFoundException('User not found');
         }
-
         $user->setResetPwdToken(bin2hex(random_bytes(32)));
         $this->em->flush();
 
         // SEND EMAIL WITH LINK AND TOKEN
-
+        $emailconfig = (new TemplatedEmail())
+            ->from(new Address('pamana@easylocmoto.fr','Panama Agency'))
+            ->to($email)
+            ->subject('Reset Your Password')
+            ->htmlTemplate('template/email/Reset-password.html.twig')
+            ->context(['name'=> $user->getSurname(),
+                        'token' => $user->getResetPwdToken()]);
+        $mailer->send($emailconfig);
 
         return $this->json("Success");
     }
